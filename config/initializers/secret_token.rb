65ad2fbc918f51fa6409d645ad56f5e1dee03a40
vibin.nair@gmail.com <vibin.nair@gmail.com>
2012-05-19 08:18:29 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cf60b8f8f71ba3b38c4ffcc5b65cc5a07db792560bec00dccfe91a9d73a483c895189121ca52829339b80bbaaa7fa39158ba5e9a4856a4c83e4f028a65b724a0'
